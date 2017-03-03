199057a68e37152dadc6bd2c672bcf88f299f837
tylerku <ty.udy22@gmail.com>
2017-03-03 15:55:34 -0700

    Create New Command For Server
    Basically what the title says

---
+++ ttrServer/com/example/tyudy/ticket2rideclient/common/commands/InitializeDestinationCardsCommand.java
@@ -0,0 +1,24 @@
+package com.example.tyudy.ticket2rideclient.common.commands;
+
+import com.example.tyudy.ticket2rideclient.common.Command;
+import com.example.tyudy.ticket2rideclient.common.DataTransferObject;
+import com.example.tyudy.ticket2rideclient.common.iCommand;
+
+import java.io.Serializable;
+
+/**
+ * Created by tyudy on 3/3/17.
+ */
+public class InitializeDestinationCardsCommand extends Command implements iCommand, Serializable {
+    private DataTransferObject data;
+
+    @Override
+    public void setData(DataTransferObject d) {
+        data = d;
+    }
+
+    @Override
+    public DataTransferObject execute() {
+        return null;
+    }
+}
