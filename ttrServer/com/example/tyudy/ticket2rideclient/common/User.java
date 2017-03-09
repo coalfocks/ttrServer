f8a00a8a9639a210bafccb8050afca38d2942d87
zacheat <zacheat@gmail.com>
2017-03-08 19:27:01 -0700

    User Updated
    The user was updated so hard.

--- ttrServer/com/example/tyudy/ticket2rideclient/common/User.java
+++ ttrServer/com/example/tyudy/ticket2rideclient/common/User.java
@@ -1,21 +1,27 @@
 package com.example.tyudy.ticket2rideclient.common;
 
+import com.example.tyudy.ticket2rideclient.common.cards.DestinationCard;
 import com.example.tyudy.ticket2rideclient.common.cards.TrainCard;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Map;
 
 /**
  * Created by tyudy on 2/7/17.
  */
 
-public class User implements Serializable {
+public class User implements Serializable, Comparable<User> {
     private String username;
     private String password;
     private int playerID;
     private int inGame;
-    private int color;
     private int points = 0;
+
+    private Color color;
+    private User associatedUser;
+    private Map<Color, TrainCard> colorCards;
+    private ArrayList<DestinationCard> destCards;
 
 
     public User()
@@ -25,7 +31,6 @@ public class User implements Serializable {
         playerID = 0;
         inGame = 0;
         points = 0;
-        color = 0;
     }
 
     public User(String username, String password, int playerID, int inGame)
@@ -77,15 +82,6 @@ public class User implements Serializable {
         this.username = username;
     }
 
-    public int getColor()
-    {
-        return color;
-    }
-
-    public void setColor(int color)
-    {
-        this.color = color;
-    }
 
     public int getPoints()
     {
@@ -95,4 +91,71 @@ public class User implements Serializable {
     public void addPoints(int pointValue) {
         this.points += pointValue;
     }
+
+    @Override
+    public int compareTo(User o)
+    {
+        if(this.playerID > o.getPlayerID()) {
+            return 1;
+        }
+        else if (this.playerID < o.getPlayerID()) {
+            return -1;
+        }
+        return 0;
+    }
+
+    public boolean addDestinationCard(DestinationCard card){
+        return destCards.add(card);
+    }
+
+    public ArrayList<DestinationCard> getDestCards() { return destCards; }
+
+    //Cards stuff
+    public void addTrainCard(TrainCard card){
+        TrainCard c = colorCards.get(card.getColor());
+        if(c != null) {
+            c.incNum();
+            colorCards.put(card.getColor(), c);
+        }
+        else{
+            colorCards.put(card.getColor(), card);
+        }
+    }
+    public ArrayList<TrainCard> getTrainCards(){
+        ArrayList<TrainCard> arrayOfCards = new ArrayList<TrainCard>(colorCards.values());
+        return arrayOfCards;
+    }
+
+    public TrainCard getNumCardsOfColor(Color c) { return colorCards.get(c); }
+
+
+    public void increasePoints(int addPoints) {
+        points += Math.abs(addPoints);
+    }
+
+    public void decreasePoints(int subtractPoints) {
+        points -= Math.abs(subtractPoints);
+
+        // Can't go less than 0 points
+        if (points < 0)
+            points = 0;
+    }
+
+    public void setPoints(int newValue) {
+        if (newValue > 0)
+            points = newValue;
+        else
+            points = 0;
+    }
+
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+
 }
