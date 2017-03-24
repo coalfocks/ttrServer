453e0ed1cbbf944da8a2dbca8854419baddd25e1
tylerku <ty.udy22@gmail.com>
2017-03-24 15:43:33 -0600

    Working
    &&&

--- ttrServer/com/example/tyudy/ticket2rideclient/common/TTRGame.java
+++ ttrServer/com/example/tyudy/ticket2rideclient/common/TTRGame.java
@@ -134,16 +134,6 @@ public class TTRGame implements Serializable
 
     }
 
-    // dealTrainCard used by the server
-    public void dealTrainCard(int playerID){
-        TrainCard card = (TrainCard) myTrainDeck.getCard();
-        for (User u : players) {
-            if (u.getPlayerID() == playerID) {
-                u.addTrainCard(card);
-            }
-        }
-    }
-
 
     public TrainCard dealTrainCard(int playerID){
         TrainCard myCard = null;
