5ceee70c00272200de9b4f5cc5453a9f51ae1b0d
Master Owl <cheesytrevor@gmail.com>
2017-04-17 15:01:17 -0600

    Created methods for Game and User DAO interfaces

--- ttrServer/server/Database/dao/IGameDAO.java
+++ ttrServer/server/Database/dao/IGameDAO.java
@@ -1,8 +1,30 @@
 package server.Database.dao;
 
+import com.example.tyudy.ticket2rideclient.common.TTRGame;
+
+import java.util.ArrayList;
+
+import server.Database.Database;
+
 /**
  * Created by Trevor on 4/17/2017.
  */
 
 public interface IGameDAO {
+
+    boolean addChatMessage(String username, int gameID, String message);
+    boolean reset();
+    boolean setDB(Database database);
+    boolean endGame(int ownerID);
+    boolean updateGame(TTRGame game);
+    TTRGame createGame(int ownerID);
+    TTRGame getGame(int gameID);
+    TTRGame getGameByOwner(int ownerID);
+    TTRGame startGame(int ownerID);
+    int getGameID(String game);
+    int getGameStatus(int gameID);
+    int getNumPlayers(int gameID);
+    ArrayList<TTRGame> getGames();
+    Database getDB();
+    IGameDAO getInstance();
 }
