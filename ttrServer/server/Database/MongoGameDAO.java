9d7ea9335592e1732bf306210e951d2390d8909d
coalfocks <coalfocks@gmail.com>
2017-04-18 21:42:49 -0600

    started game daos

--- ttrServer/server/Database/MongoGameDAO.java
+++ ttrServer/server/Database/MongoGameDAO.java
@@ -1,7 +1,10 @@
 package server.Database;
 
 import com.example.tyudy.ticket2rideclient.common.TTRGame;
+import com.example.tyudy.ticket2rideclient.common.User;
+import com.mongodb.*;
 import server.interfaces.IGameDAO;
+import server.interfaces.IUserDAO;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -11,22 +14,56 @@ import java.util.ArrayList;
  */
 public class MongoGameDAO implements IGameDAO {
 
+    private static final MongoGameDAO instance = new MongoGameDAO();
+    private IUserDAO userDAO = DAOHolder.getInstance().getUserDAO();
+    private DB mongoDB;
+    private DBCollection gamesCollection;
+    private DBCollection chatsCollection;
+
+    private MongoGameDAO() {
+        mongoDB = DAOHolder.getInstance().getDb();
+        gamesCollection = mongoDB.getCollection("games");
+        chatsCollection = mongoDB.getCollection("chats");
+    }
+
+    private IGameDAO getInstance () {
+        return instance;
+    }
+
     @Override
     public int createGame(int ownerID)
     {
-        return 0;
+        if (ownerID == 0) {
+            return 0;
+        }
+
+        User u = userDAO.getUser(ownerID);
+        TTRGame game = new TTRGame();
+        game.setOwnerID(ownerID);
+        game.setInProgress(0);
+        int count = (int) gamesCollection.getCount() + 1;
+        game.setGameID(count);
+        game.setOwnerUsername(u.getUsername());
+        DBObject obj = MongoObjectConverter.SINGLETON.gameToDBObject(game);
+        gamesCollection.insert(obj);
+        return count;
     }
 
     @Override
     public TTRGame getGame(int gameID)
     {
-        return null;
+        DBObject gameQuery = new BasicDBObject("_id", gameID);
+        DBCursor gameCursor = gamesCollection.find(gameQuery);
+        DBObject obj = gameCursor.one();
+        return MongoObjectConverter.SINGLETON.dbObjectToGame(obj);
     }
 
     @Override
     public ArrayList<TTRGame> getGames(int gameID)
     {
-        return null;
+        DBObject progressQuery = new BasicDBObject("inProgress", 0);
+        DBObject idQuery = new BasicDBObject("game", gameID);
+        ArrayList<TTRGame> games = gamesCollection.find($or:)
     }
 
     @Override
